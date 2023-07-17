0cc5061cadddb5443738aa814b4917ae28c42702
kennyhalling <kennyhalling@gmail.com>
2023-07-16 19:09:26 -0500

    What I have so far

--- final/FinalProject/Menu.cs
+++ final/FinalProject/Menu.cs
@@ -1,13 +1,18 @@
+using System.IO;
+
 public class Menu{
     private int _userInput;
-    private Dictionary<string, string[]> _combatants = new Dictionary<string, string[]>();
+    private Dictionary<string, string[]> _enemyDictionary = new Dictionary<string, string[]>();
+    private Dictionary<string, int[]> _combatants = new Dictionary<string, int[]>();
     private bool _active = true;
+    private string[] _lines;
 
     public Menu(){
 
     }
 
     public void DisplayMenu(){
+        CreateEnemyDictionary();
         while (_active){
             Console.Clear();
             Console.Write(@$"
@@ -20,7 +25,8 @@ public class Menu{
             Your Choice: ");
             _userInput = int.Parse(Console.ReadLine());
             if (_userInput == 1){
-
+                Console.Clear();
+                CreateCombatantList();
             }
             else if (_userInput == 2){
                 
@@ -34,6 +40,58 @@ public class Menu{
             else{
                 
             }
+        }
+    }
+    public void CreateCombatantList(){
+        bool _addCombatants = true;
+        while (_addCombatants){
+            Console.Write(@"
+            Who would you like to add?:
+            1. Normal Enemy
+            2. Spellcasting Enemy
+            3. Quit
+            Your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine(choice);
+            if (choice == 1){
+                Console.Write(@"
+                What is the enemy's name?: ")
+                string _enemyName = Console.ReadLine()
+                Console.Write(@"
+                How many would you like to add?: ")
+                string _enemyQuantity = int.Parse(Console.ReadLine())
+                if (_enemyDictionary.ContainsKey(_enemyName)){
+
+                }
+                else{
+                    Console.WriteLine(@"
+                    Sorry, this enemy doenst exist in our files!")
+                }
+            }
+            else if (choice == 2){
+
+            }
+            else if (choice == 3){
+                Console.WriteLine(@"
+                Your final combatant list:")
+                _addCombatants = false;
+                Thread.Sleep(4000)
+            }
+            else{
+
+            }
+        }
+    }
+    public void CreateEnemyDictionary(){
+        _lines = System.IO.File.ReadAllLines("enemies.txt");
+        foreach (string line in _lines){
+            string[] _line = line.Split(",");
+            _enemyDictionary.Add(_line[0].Trim('"'), _line);
+        }
+    }
+    public void DisplayDictionary(){
+        foreach (KeyValuePair<string, string[]> entry in _enemyDictionary){
+            Console.WriteLine($"{entry.Key}: {entry.Value}");
         }
     }
 }
